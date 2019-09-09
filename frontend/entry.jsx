75a3d63fb2de8442e9c6dbc15b6c7862b6382479
Lam Vu <lvu2548@gmail.com>
2019-09-09 16:30:12 -0700

    Created project and made User model/controller

---
+++ frontend/entry.jsx
@@ -0,0 +1,7 @@
+import react from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    let root = document.getElementById('root');
+    ReactDOM.render(<Root />, root)
+})
