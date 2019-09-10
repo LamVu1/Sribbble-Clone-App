ffed3334548dd0f5e5e5cc093d254cae4c1c28dc
Lam Vu <lvu2548@gmail.com>
2019-09-10 09:55:07 -0700

    Completed RootReducer and Store, created App and Root components

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as api from './utils/session_api_util';
+import configureStore from './store/store';
 
 window.login=api.login;
 window.signup=api.signup;
 window.signout=api.logout;
 document.addEventListener('DOMContentLoaded', ()=>{
+    const store = configureStore();
+    window.getState = store.getState;
+    window.dispatch = store.dispatch; 
     let root = document.getElementById('root');
-    ReactDOM.render(<h1>rerereere</h1>, root)
+    ReactDOM.render(<h1>React is working</h1>, root)
 })
