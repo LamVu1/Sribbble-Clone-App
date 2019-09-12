574ec628cdfc2984bbdf2ee648c4d477e41ada0b
Lam Vu <lvu2548@gmail.com>
2019-09-12 09:27:48 -0700

    Update signup form CSS

--- frontend/components/session_form/signup_form.jsx
+++ frontend/components/session_form/signup_form.jsx
@@ -44,25 +44,38 @@ class SignupForm
 
     return (
       <div className="signup-form-container">
-        <div className="signup-header">
-          <h2 className="signup-header-span">JOIN THE SCRIBBLEZ COMMUNITY</h2>
-          <h2 className="signup-header-h2">Sign Up</h2>
+        <div className="signup-header-container">
+          <div className="signup-header">
+          <h2 className="signup-header-head1">JOIN THE SCRIBBLEZ COMMUNITY</h2>
+          <h2 className="signup-header-head2">Sign Up</h2>
+          </div>
         </div>
 
         <form onSubmit={this.handleSubmit} className="login-form-box">
-         
+         <div className="login-form-box-inner">
           <div className="login-form">
+            <h2 className="signup-form-h2">Discover the worlds top designers & creatives</h2>
+            <p className="p-tag">The go-to destination for discovering and connecting with designers and creative talent around the globe.</p>
           {this.renderErrors()}
             <br/>
-            <label>Username or Email
+            <label>Username
               <br/>
               <input type="text"
                 value={this.state.username}
                 onChange={this.handleUpdate('username')}
                 className="login-input"
               />
+              <p>Your Dribbble URL: https://dribbble.com/USERNAME</p>
             </label>
             <br/>
+            <div>
+             <label>email:
+               <br/>
+            <input type="text" value={this.state.email}
+                onChange={this.handleUpdate('email')}
+                className="login-input"/>
+            </label>
+            </div>
             <label>Password
               <br/>
               <input type="password"
@@ -71,16 +84,10 @@ class SignupForm
                 className="login-input"
               />
             </label>
+            <p>Minimum 6 characters</p>
             <br/>
-            <div>
-             <label>email:
-            <input type="text" value={this.state.email}
-                onChange={this.handleUpdate('email')}
-                className="login-input"/>
-            </label>
-            </div>
             <input className="session-submit" type="submit" value='Create Account' />
-            
+            </div>
           </div>
         </form>
       </div>
