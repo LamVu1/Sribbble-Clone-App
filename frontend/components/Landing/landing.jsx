7bd135acda79af4a509d52a5307fe9a27f808131
Lam Vu <lvu2548@gmail.com>
2019-09-13 09:11:06 -0700

    Updating landing page banner

--- frontend/components/Landing/landing.jsx
+++ frontend/components/Landing/landing.jsx
@@ -16,9 +16,18 @@ class LandingPage extends React.Component {
   return(
     <div className="main-page-container">
       <div className="main-banner">
+        <div className="main-banner-container">
         <div className="image-container"> 
+          <div className="image-text">
+              <h1>Discover the world's top designers & creatives</h1>
+              <p>Scribbble is the leading destination to find & showcase creative work and home to the world's best design professionals.</p>
+          </div>
           <img src={cactus.img} className='main-page-img'/> 
         </div>
+        <div className="image-credit">
+          <p>Art by </p>
+        </div>
+      </div>
       </div>
     </div>
   )   
