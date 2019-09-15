61a4461acb5c97dcb0ccbb1ca18c75721d99643d
Lam Vu <lvu2548@gmail.com>
2019-09-15 00:50:49 -0700

    Updated nagivation bar styling

--- frontend/components/nav/main_nav.jsx
+++ frontend/components/nav/main_nav.jsx
@@ -18,24 +18,35 @@ class Nav extends React.Component {
     render(){
     let sessionLinks;
     if(this.props.currentUser){
-        sessionLinks = <hgroup className="header-group">
-        <h2 className="header-name">Hi, {this.props.currentUser.username}!</h2>
-        <button className="header-button" onClick={this.handleSubmit}>Log Out</button>
-    </hgroup>}
+        sessionLinks = 
+            <hgroup className="nav-group">
+                <h2 className="nav-name">
+                    Hi, {this.props.currentUser.username}!
+                </h2>
+                <button className="nav-logout-btn" onClick={this.handleSubmit}>
+                    Log Out
+                </button>
+            </hgroup>}
     else{
-        sessionLinks= <nav className="main-nav">
-        <Link to="/login" className="main-nav-signin">Sign in</Link>
-        <button className="nav-signup-btn">
-        <Link to="/signup" className="main-nav-signup">Sign Up</Link>
-        </button>
-         </nav>
+        sessionLinks= 
+        <nav className="nav-group">
+            <Link to="/login" className="nav-signin">
+                Sign in
+            </Link>
+            <button className="nav-signup-btn">
+                <Link to="/signup"  className="main-nav-signup">
+                    Sign Up
+                </Link>
+            </button>
+        </nav>
     }
 
     return(
     <div className="Main-nav">
-        <div  className='main-nav-logo-cont'>
-        <Link to='/'>
-          <img src={window.img2} className='main-nav-logo'/></Link>
+        <div  className='nav-logo-container'>
+            <Link to='/'>
+                <img src={window.img2} className='main-nav-logo'/>
+            </Link>
         </div>
         {sessionLinks}
     </div>
