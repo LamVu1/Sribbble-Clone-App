42b6a81beed7b79983278df8fc1d8861794fd714
Lam Vu <lvu2548@gmail.com>
2019-10-31 15:42:18 -0700

    Follow feature working

--- frontend/components/follows/follow.jsx
+++ frontend/components/follows/follow.jsx
@@ -5,21 +5,29 @@ import {calcTime} from '../../utils/calculate_time';
 class FollowIndex extends React.Component{
     constructor(props) {
         super(props);
-        
-
+        this.handleFollow=this.handleFollow.bind(this);
+        this.handleUnfollow=this.handleUnfollow.bind(this);
         }
 
-        
+    handleFollow(){
+        this.props.createFollow(this.props.author_id)
+    }
+
+    handleUnfollow(){
+        let id;
+        id=this.props.follows.filter(follow=>follow.follower_id===this.props.currentuser_id)
+        this.props.deleteFollow(id[0].id)
+    }
       
     render(){
-        
-        
         return(
             <div>
                <h1>Follows</h1>
                 <div>
-                    {(this.props.follows.filter(follow=>follow.follower_id===this.props.currentuser_id).length===0)
-                    ?<h1>NO</h1>:<h1>YES</h1>
+                    {this.props.follows.filter(follow=>follow.follower_id===this.props.currentuser_id).length===0
+                    
+                    ?<button onClick={this.handleFollow}>Follow</button>
+                    :<button onClick={this.handleUnfollow}>Unfollow</button>
             
                 }
                 </div>
