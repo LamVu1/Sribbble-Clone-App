2d8ad2e918622c4f3ef687c02273a141aebb2728
Lam Vu <lvu2548@gmail.com>
2020-10-17 03:22:53 -0700

    Updated banner

--- frontend/pages/images.js
+++ frontend/pages/images.js
@@ -1,13 +1,12 @@
 export const image={0:{
-    img: "https://cdn.dribbble.com/assets/art-banners/cactus4x3-00b2c882ddc01c9d5ebe19c886dc2c87bd18ef7aa1cdc1c53980fc305bcecfca.jpg",
+    img: "https://app-ecommerce-seeds.s3-us-west-1.amazonaws.com/TimBoelaars.jpg",
     color:"#E3A550",
     tcolor1: "#2F612C",
     tcolor2: "#A93D14",
     author: "Tim Boelaars in Amsterdam"
 },
-1:{img: "https://cdn.dribbble.com/assets/art-banners/spaceman-b6754ff469f369e504a98de09e34a6749b6e3536dfa07e4ae4637b6ac02afdad.png", color: "#FFFFFF", tcolor1: "#6393D2", tcolor2: "#B8B8B8", author: "Eddie Lobanovskiy in Sarasota, FL"
-},
-2:{img: "https://cdn.dribbble.com/assets/art-banners/botw-c092192e602abaa8d6a18c807d25a5f84d57e52eb907d3fa820fb83d29554e37.png", 
+1:{img: "https://app-ecommerce-seeds.s3-us-west-1.amazonaws.com/spaceman.png", color: "#FFFFFF", tcolor1: "#6393D2", tcolor2: "#B8B8B8", author: "Eddie Lobanovskiy in Sarasota, FL"},
+2:{img: "https://app-ecommerce-seeds.s3-us-west-1.amazonaws.com/link.png", 
 color: "#F5EBC9", tcolor1: "#37055A", tcolor2: "#E63F4B", author: "Marie Bergeron in Montreal, Qc, Canada"},
-3:{img: "https://cdn.dribbble.com/assets/art-banners/itsa-me-time_dennis-salvatier-b841932f47297051d03e315df1df0d93e7c20bc065540182cb0554a8184bd3cc.jpg", color: "#FDFEEC", tcolor1: "#31759A", tcolor2: "#FF2D6E", author: "Dennis Salvatier in Los Angeles, CA"},
-4:{img: "https://cdn.dribbble.com/assets/signup-banners/paper-camera-503a3c3694e59748eca4b4da479b2caad8ff77f6f100c49b24753b689072f6e6.jpg", color: "#D4D8DB", tcolor1: "#A49086", tcolor2:"#C37865", author: "Alicja Colon / Thrice Studio in Savannah, Georgia"}}
+3:{img: "https://app-ecommerce-seeds.s3-us-west-1.amazonaws.com/romainbriaux.png", color: "#F9F8FD", tcolor1: "#333333", tcolor2: "#333333", author: "Romain Briaux"},
+4:{img: "https://app-ecommerce-seeds.s3-us-west-1.amazonaws.com/alicjacolon.jpg", color: "#D4D8DB", tcolor1: "#A49086", tcolor2:"#C37865", author: "Alicja Colon / Thrice Studio in Savannah, Georgia"}}
